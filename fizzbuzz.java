925510ffec2722d7f133818f5654a61136cdd3ef
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-17 15:08:25 +0530

    Create fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 50) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } 
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } 
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } 
+            else {
+                System.out.println(i);
+            }
+
+            i++;
+        }
+    }
+}

54d809288ea500b33114613f0a1e02a4f7726fb5
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-17 15:19:03 +0530

    Create numberpattern.java

---
+++ numberpattern.java
@@ -0,0 +1,14 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = n; i >= 1; i--) {
+            for (int s = 0; s < n - i; s++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}

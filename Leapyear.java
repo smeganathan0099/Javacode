a46b3548c0ad857d9afb3a8524ee974a4d0758d3
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-17 14:29:19 +0530

    Create Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int i=2005;
+        while(i<=2026){
+                if(i%4==0){
+            System.out.println("leap year");
+            i++;
+            }
+                    else{
+        System.out.println(i);
+        i++;}
+    }
+    }
+}

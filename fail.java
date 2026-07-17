e47dc9a578f56b7970d1f09036838d7759893f8b
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-17 14:48:56 +0530

    Create fail.java

---
+++ fail.java
@@ -0,0 +1,12 @@
+public class PassFail {
+    public static void main(String[] args) {
+
+        int marks = 45;
+
+        if (marks >= 50) {
+            System.out.println("pass");
+        } else {
+            System.out.println("fail");
+        }
+    }
+}

23d1d0038912b1ff58cc2b9eab2d57d352718f51
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-17 14:25:43 +0530

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,2 +1,25 @@
 # Javacode
 Placement Training Class -JAVA
+public class Main{
+    public static void main(String[] args)
+    {
+        int i=1;
+        while(i<=50){
+                if(i%3==0&&i%5==0){
+            System.out.println("Fizz Buzz");
+            i++;
+            }
+            else if(i%3==0){
+                    System.out.println("Fizz");
+                            i++;
+                }
+                else if(i%5==0){
+                        System.out.println("Buzz");
+                        i++;
+                    }
+                    else{
+        System.out.println(i);
+        i++;}
+    }
+    }
+}

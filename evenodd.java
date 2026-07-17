dbb4d7b884d1dd277f2d89364f8e67f408e77351
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-17 14:41:31 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        int numbers = 8;
+
+        if (numbers % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+}

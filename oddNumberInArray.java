0f860c3d3f1db8dc2f6d56585fa2ec18c7c84823
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-20 15:31:23 +0530

    Create oddNumberInArray.java

---
+++ oddNumberInArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class oddNumberInArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+System.out.print("Enter the number of element in a array:");
+int n= sc.nextInt();
+int[]arr=new int[n];
+for(int i=0;i<n;i++){
+System.out.print("Enter the"+(i+1)+"Element:");
+arr[i]=sc.nextInt();
+}
+System.out.println("odd element are:");
+for(int i=0;i<n;i++)
+{
+    if(arr[i]%2==1)
+    {
+        System.out.println(arr[i]+"");
+}
+    } 
+    }
+}

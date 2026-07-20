1b20add3f7faf746475c5e4dd897ac2d27408f93
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-20 14:28:45 +0530

    Implement array reversal and print elements

---
+++ ReverseTheArray.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+int[]arr={10,20,30,15,40};
+for(int i=arr.length-1;i>0;i--)
+{
+System.out.println(arr[i]+"");
+}
+}
+}

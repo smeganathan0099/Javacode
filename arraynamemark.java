b5799856152e4bbf6647868b2c4d40854e47b50d
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-20 16:33:34 +0530

    Create arraynamemark.java

---
+++ arraynamemark.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = {350,450,498,415,445,491,379,479,389,371};
+        String[] name = {"Sam", "Thivi", "Sanvi","Indhu","Raju",
+            "Mega","Yogesh","Janaki","Prasath","Dharshana" };
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(name[i] + " = " + marks[i]);
+        }
+    }
+}

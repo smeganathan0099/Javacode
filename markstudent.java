8dca2ff1d836f604dae0997d65b2c78c1f0f67ab
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-21 14:53:17 +0530

    Create markstudent.java

---
+++ markstudent.java
@@ -0,0 +1,19 @@
+public class students{
+    public static void main(String[]args){
+        String[] name={"Indhu","sanvi","mega","Dharshu"};
+        int[][] mark={{66,78,98,89,88},{97,77,69,89,80},{90,80,70,60,99},{100,100,100,100,100}};
+        for(int i=0;i<name.length;i++)
+        {
+            System.out.println("Name="+ name[i]);
+            double total=0;
+            for(int j=0;j<mark[i].length;j++)
+            {
+                System.out.print(mark[i][j]+" ");
+                total +=mark[i][j];
+    }
+    System.out.println();
+    System.out.println("Average:"+(total/5));
+    System.out.println();
+}
+}
+}

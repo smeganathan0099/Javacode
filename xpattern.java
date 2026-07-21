de01e8e6904ed9a9fb3850a3becfc6168286b2bd
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-21 14:24:29 +0530

    Create xpattern.java

---
+++ xpattern.java
@@ -0,0 +1,15 @@
+public class XPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i || j == n - 1 - i)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}

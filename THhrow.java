6e5ffae3112e35f523d0927c5fc21944c6059c74
smeganathan0099 <smeganathan0099@gmail.com>
2026-07-28 15:08:13 +0530

    Create THhrow.java

---
+++ THhrow.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int age = 15;
+
+        if (age < 18) {
+            throw new ArithmeticException("Not eligible to vote");
+        }
+
+        System.out.println("Eligible to vote");
+    }
+}
